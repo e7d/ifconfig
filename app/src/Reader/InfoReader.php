b66dce1492983681cc39096dc1608ac19580e825
e7d <michael@e7d.io>
2020-01-24 15:29:07 +0100

    early processing of CloudFlare ip

--- app/src/Reader/InfoReader.php
+++ app/src/Reader/InfoReader.php
@@ -45,11 +45,11 @@ class InfoReader
 
     private function readIpFromHeaders(array $headers): string
     {
-        if (isset($headers['HTTP_X_REAL_IP']) && filter_var($headers['HTTP_X_REAL_IP'], FILTER_VALIDATE_IP)) {
-            return $headers['HTTP_X_REAL_IP'];
-        }
         if (isset($headers['HTTP_CF_CONNECTING_IP']) && filter_var($headers['HTTP_CF_CONNECTING_IP'], FILTER_VALIDATE_IP)) {
             return $headers['HTTP_CF_CONNECTING_IP'];
+        }
+        if (isset($headers['HTTP_X_REAL_IP']) && filter_var($headers['HTTP_X_REAL_IP'], FILTER_VALIDATE_IP)) {
+            return $headers['HTTP_X_REAL_IP'];
         }
         if (isset($headers['HTTP_CLIENT_IP']) && filter_var($headers['HTTP_CLIENT_IP'], FILTER_VALIDATE_IP)) {
             return $headers['HTTP_CLIENT_IP'];
