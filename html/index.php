a066259dc41dd56601154165b6cbd83537a37353
e7d <michael@e7d.io>
2020-02-11 17:59:43 +0100

    handle error display

--- html/index.php
+++ html/index.php
@@ -2,6 +2,11 @@
 
 use IfConfig\Application;
 
+if ($_ENV['mode'] !== 'DEBUG') {
+    ini_set("display_errors", 0);
+    ini_set("log_errors", 1);
+}
+
 require __DIR__ . '/../vendor/autoload.php';
 
 new Application();
