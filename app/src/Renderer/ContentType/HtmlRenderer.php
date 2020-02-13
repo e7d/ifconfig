814105f669d8d6978b171dd2c882c5a50040a4a6
e7d <michael@e7d.io>
2020-02-13 18:00:06 +0100

    use object info for html rendering

--- app/src/Renderer/ContentType/HtmlRenderer.php
+++ app/src/Renderer/ContentType/HtmlRenderer.php
@@ -2,12 +2,17 @@
 
 namespace IfConfig\Renderer\ContentType;
 
-use IfConfig\Types\Info;
 use IfConfig\Types\Location;
 
 class HtmlRenderer extends ContentTypeRenderer
 {
-    private function arrayToString(array $array): string
+    private function getLocationString(Location $location): string
+    {
+        $coordinates = $location->getLatitude() . ', ' . $location->getLongitude();
+        return '<a href="https://www.openstreetmap.org/?mlat=' . $location->getLatitude() . '&mlon=' . $location->getLongitude() . '" target="_blank">' . $coordinates . '</a>';
+    }
+
+    private function getHeadersString(array $array): string
     {
         return implode(
             '<br>',
@@ -17,28 +22,8 @@ class HtmlRenderer extends ContentTypeRenderer
         );
     }
 
-    private function getInfoAsArray(Info $info): array
-    {
-        $i = $info->toArray(false);
-        $location = $info->getLocation();
-        if (!is_null($location)) {
-            $i['coordinates'] = $this->getLocationString($location);
-            $i['timezone'] = $location->getTimeZone();
-        }
-        $i['headers'] = $this->arrayToString($i['headers']);
-        return $i;
-    }
-
-    private function getLocationString(Location $location): string
-    {
-        $coordinates = $location->getLatitude() . ', ' . $location->getLongitude();
-        return '<a href="https://www.openstreetmap.org/?mlat=' . $location->getLatitude() . '&mlon=' . $location->getLongitude() . '" target="_blank">' . $coordinates . '</a>';
-    }
-
     public function render(): void
     {
-        global $i;
-        $i = $this->getInfoAsArray($this->info);
         require_once __DIR__ . '/../Templates/index.phtml';
     }
 }
