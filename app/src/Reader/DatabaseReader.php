83cd054bcd1ea1ce445f68993777b13588daa135
e7d <michael@e7d.io>
2020-10-26 12:10:51 +0100

    proper MaxMind copyright attribution

--- app/src/Reader/DatabaseReader.php
+++ app/src/Reader/DatabaseReader.php
@@ -12,9 +12,15 @@ class DatabaseReader
         return file_exists($file) ? $file : null;
     }
 
-    public static function getDbFileDate(): int
+    public static function getDbFileTimestamp(): int
     {
         $file = self::getDbFilePath();
         return is_null($file) ? 0 : filemtime($file);
+    }
+
+    public static function getDbFileLatestUpdate(): ?string
+    {
+        $dbTimestamp = max(AsnReader::getDbFileTimestamp(), LocationReader::getDbFileTimestamp());
+        return $dbTimestamp > 0 ? date('Y/m/d', $dbTimestamp) : null;
     }
 }
