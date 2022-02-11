ef0925b16018a56d82b85ac86c33207a5b8037a0
e7d <michael@e7d.io>
2022-02-11 09:37:02 +0100

    catch dns resolution errors

--- app/src/Utils/DnsService.php
+++ app/src/Utils/DnsService.php
@@ -3,15 +3,20 @@
 namespace Utils;
 
 use Error;
+use Exception;
 
 class DnsService
 {
     public static function resolve(string $host, ?int $type = null): ?string
     {
-        if (!in_array($type, [null, DNS_A, DNS_AAAA])) {
-            throw new Error('Invalid resolve type: Acceptable values are null, DNS_A or DNS_AAAA');
+        try {
+            if (!in_array($type, [null, DNS_A, DNS_AAAA])) {
+                throw new Error('Invalid resolve type: Acceptable values are null, DNS_A or DNS_AAAA');
+            }
+            $entries = @dns_get_record($host, is_null($type) ? DNS_A + DNS_AAAA : $type);
+            return is_array($entries) && count($entries) ? ($entries[0]['ipv6'] ?? $entries[0]['ip']) : null;
+        } catch (Exception $e) {
+            return null;
         }
-        $entries = dns_get_record($host, is_null($type) ? DNS_A + DNS_AAAA : $type);
-        return is_array($entries) && count($entries) ? ($entries[0]['ipv6'] ?? $entries[0]['ip']) : null;
     }
 }
