a58fe5512dc881d561a02d5812490bc3670861b3
e7d <michael@e7d.io>
2023-03-03 10:24:53 +0100

    improve interface

--- html/js/query.js
+++ html/js/query.js
@@ -6,23 +6,23 @@ const $fieldSelect = form.querySelector("select[name=field]");
 const $submitArea = document.querySelector("span#submit");
 
 $formatSelect.addEventListener("change", () => {
-  const format = $formatSelect.value;
+  const [format, pretty] = $formatSelect.value.split('-');
   $fieldSelect.disabled = format === "html";
   if ($fieldSelect.disabled) $fieldSelect.value = "";
 });
 
 form.addEventListener("submit", (e) => e.preventDefault());
 
-function generateUrlPath(params) {
-  return params.join("/");
+function generateUrlPath(params, pretty) {
+  return `${params.join("/")}${pretty ? '?pretty' : ''}`;
 }
 
-function generateQueryPath(params) {
+function generateQueryPath(params, pretty) {
   return Object.keys(params).length === 0
     ? ""
     : `?${Object.entries(params)
-        .map(([k, v]) => `${k}=${v}`)
-        .join("&")}`;
+      .map(([k, v]) => `${k}=${v}`)
+      .join("&")}${pretty ? '&pretty' : ''}`;
 }
 
 function generateLink(uri) {
@@ -31,21 +31,23 @@ function generateLink(uri) {
   return `<a href="${link}" target="_blank">${link}</a>`;
 }
 
-function generateForm(params) {
+function generateForm(params, pretty) {
   const { protocol, host } = window.location;
   return `<form method="post" action="${protocol}//${host}/">
     ${Object.entries(params)
       .map(([k, v]) => `<input type="hidden" name="${k}" value="${v}">`)
       .join("")}
+    ${pretty ? '<input type="hidden" name="pretty" value="">' : ''}
     <input type="submit" value="Submit">
   </form>`;
 }
 
 function generateQuery() {
+  const [format, pretty] = $formatSelect.value.split('-');
   const params = Object.fromEntries(
     Object.entries({
       host: $queryInput.value,
-      format: $formatSelect.value,
+      format,
       field: $fieldSelect.value,
     }).filter(([, v]) => !!v)
   );
@@ -53,13 +55,13 @@ function generateQuery() {
   switch ($methodSelect.value) {
     default:
     case "url":
-      html = generateLink(generateUrlPath(Object.values(params)));
+      html = generateLink(generateUrlPath(Object.values(params), pretty));
       break;
     case "get":
-      html = generateLink(generateQueryPath(params));
+      html = generateLink(generateQueryPath(params, pretty));
       break;
     case "post":
-      html = generateForm(params);
+      html = generateForm(params, pretty);
       break;
   }
   $submitArea.innerHTML = html;
