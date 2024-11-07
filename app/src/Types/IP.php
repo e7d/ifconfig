b866ca8c379ebd8012756c44a052eda469c69d6c
e7d <michael@e7d.io>
2024-11-07 17:42:48 +0100

    feat(): add an IPv6 test

--- app/src/Types/IP.php
+++ app/src/Types/IP.php
@@ -11,6 +11,8 @@ class IP extends AbstractType implements JsonSerializable
     protected string $address;
     protected int|string|false $decimal;
     protected int $version;
+    protected ?string $type;
+    protected ?bool $slaac;
 
     public function __construct(string $address)
     {
@@ -20,6 +22,10 @@ class IP extends AbstractType implements JsonSerializable
         $this->address = $address;
         $this->version = filter_var($address, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) ? 6 : 4;
         $this->decimal = $this->version === 6 ? $this->ip2long_v6($address) : ip2long($address);
+        if ($this->version === 6) {
+            $this->type =$this->toIpv6Type($address);
+            $this->slaac =$this->isSlaacAddress($address);
+        }
     }
 
     private function ip2long_v6($ip): string|false
@@ -27,13 +33,52 @@ class IP extends AbstractType implements JsonSerializable
         if (!function_exists('gmp_init')) {
             return false;
         }
-
         $packedIp = inet_pton($ip);
         $bin = '';
         for ($bit = strlen($packedIp) - 1; $bit >= 0; $bit--) {
             $bin = sprintf('%08b', ord($packedIp[$bit])) . $bin;
         }
         return gmp_strval(gmp_init($bin, 2), 10);
+    }
+
+    private function is6to4Address($ipv6)
+    {
+        if (filter_var($ipv6, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) === false) {
+            return false;
+        }
+        $binaryIPv6 = inet_pton($ipv6);
+        $firstTwoBytes = unpack("n", $binaryIPv6)[1];
+        return ($firstTwoBytes === 0x2002);
+    }
+
+    private function isTeredoAddress($ipv6)
+    {
+        if (filter_var($ipv6, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) === false) {
+            return false;
+        }
+        $binaryIPv6 = inet_pton($ipv6);
+        $firstFourBytes = unpack("N", $binaryIPv6)[1];
+        return ($firstFourBytes === 0x20010000);
+    }
+
+    private function toIpv6Type($ip): string
+    {
+        if ($this->is6to4Address($ip)) {
+            return '6to4';
+        }
+        if ($this->isTeredoAddress($ip)) {
+            return 'teredo';
+        }
+        return 'native';
+    }
+
+    private function isSlaacAddress($ipv6)
+    {
+        if (filter_var($ipv6, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) === false) {
+            return false;
+        }
+        $binaryIPv6 = inet_pton($ipv6);
+        return ord($binaryIPv6[11]) == 0xFF && ord($binaryIPv6[12]) == 0xFE;
     }
 
     public function __toString(): string
