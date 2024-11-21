67ef1d6d79066b3f149cf2ba5f591d81b61a362b
e7d <michael@e7d.io>
2024-11-21 23:35:38 +0100

    fix GeoLite2 City download

--- docker/resources/entrypoint.sh
+++ docker/resources/entrypoint.sh
@@ -23,7 +23,7 @@ else
 	echo "A recent enough MaxMind GeoLite2 ASN database already exists. Skipping download."
 fi
 
-if [ ! -z "$MAXMIND_LICENSE_KEY" ] && { [ ! -f "$DATABASE_DIR/GeoLite2-ASN.mmdb" ] || [ ! -z "$(find "$DATABASE_DIR/GeoLite2-ASN.mmdb" -mtime +1)" ]; }; then
+if [ ! -z "$MAXMIND_LICENSE_KEY" ] && { [ ! -f "$DATABASE_DIR/GeoLite2-City.mmdb" ] || [ ! -z "$(find "$DATABASE_DIR/GeoLite2-City.mmdb" -mtime +1)" ]; }; then
 	mkdir -p $DATABASE_DIR
 	echo -ne "Downloading MaxMind GeoLite2 City database... "
 	wget -qO- "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=$MAXMIND_LICENSE_KEY&suffix=tar.gz" | tar xz --directory /tmp
